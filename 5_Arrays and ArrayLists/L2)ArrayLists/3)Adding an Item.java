93f2f3ab07d580651e1c6c3304d4736c517589bd
KOKI <agiks21@gmail.com>
2020-10-21 18:32:53 +0900

    ArrayLists

---
+++ 5_Arrays and ArrayLists/L2)ArrayLists/3)Adding an Item.java
@@ -0,0 +1,17 @@
+//Learn
+    Now we have an empty ArrayList, but how do we get it to store values?
+    ArrayList comes with an add() method that takes an argument to add to the end of the ArrayList:
+        ArrayList<Integer> sudokuRow1 = new ArrayList<Integer>();
+        sudokuRow1.add(4);
+        // sudokuRow1 now holds [4]
+        sudokuRow1.add(8);
+        // sudokuRow1 now holds [4, 8]
+        sudokuRow1.add(3);
+        // sudokuRow1 now holds [4, 8, 3]
+Instructions
+    1.
+    We’ve created an empty ArrayList called toDoList. Time to add some to-dos!
+    Below where we’ve initialized toDo1, initialize two new String variables: toDo2 and toDo3.
+    Set their values to any tasks you like.
+    2.
+    Use .add() to add toDo1, toDo2, and toDo3 to toDoList.
